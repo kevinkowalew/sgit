cd45ea939a4e2b7965e92bfabcbde4c537446968
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2023-07-16 20:25:19 -0500

    refactors test cases for simpler expectation definitions

--- internal/cmd/cmd_test.go
+++ internal/cmd/cmd_test.go
@@ -11,13 +11,14 @@ import (
 )
 
 var (
-	FULL_NAME = "repo"
-	SSH_URL   = "repo.com"
-	ERROR     = errors.New("failure")
-	MOCK_REPO = types.GithubRepository{FullName: FULL_NAME, SshUrl: SSH_URL}
-	BASE_PATH = "/Users/me"
-	FULL_PATH = "/Users/me/python/repo"
-	PYTHON    = "python"
+	FULL_NAME     = "repo"
+	SSH_URL       = "repo.com"
+	ERROR         = errors.New("failure")
+	MOCK_REPO     = types.GithubRepository{FullName: FULL_NAME, SshUrl: SSH_URL}
+	BASE_DIR      = "/Users/me"
+	LANG_BASE_DIR = "/Users/me/python"
+	FULL_PATH     = "/Users/me/python/repo"
+	PYTHON        = "python"
 )
 
 func TestRefresh(t *testing.T) {
@@ -26,52 +27,52 @@ func TestRefresh(t *testing.T) {
 	mockGithubClient := mock_interfaces.NewMockGithubClient(ctrl)
 	mockGitClient := mock_interfaces.NewMockGitClient(ctrl)
 	mockFilesystem := mock_interfaces.NewMockFilesystemClient(ctrl)
-	cmd := newRefreshCommand(mockGithubClient, mockGitClient, mockFilesystem, BASE_PATH)
+	cmd := newRefreshCommand(mockGithubClient, mockGitClient, mockFilesystem, BASE_DIR)
 
 	type TestCase struct {
 		description   string
-		defineExpects func(*mock_interfaces.MockGithubClient, *mock_interfaces.MockGitClient, *mock_interfaces.MockFilesystemClient)
+		defineExpects func()
 		assertState   func(*repositoryState) bool
 	}
 
 	for _, tc := range []TestCase{
 		{
 			description: "Forwards error from failed GetPrimaryLangaugeForRepo call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return("", ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return("", ERROR)
 			},
 		},
 		{
 			description: "Forwards error from failed directory exists call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(false, ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(false, ERROR)
 			},
 		},
 		{
 			description: "Forwards error from failed create directory call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(false, nil)
-				mfc.EXPECT().CreateDirectory(FULL_PATH).Return(ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(false, nil)
+				mockFilesystem.EXPECT().CreateDirectory(FULL_PATH).Return(ERROR)
 			},
 		},
 		{
 			description: "Forwards error from failed clone repo call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(false, nil)
-				mfc.EXPECT().CreateDirectory(FULL_PATH).Return(nil)
-				mgc.EXPECT().CloneRepo(MOCK_REPO, FULL_PATH).Return(ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(false, nil)
+				mockFilesystem.EXPECT().CreateDirectory(FULL_PATH).Return(nil)
+				mockGitClient.EXPECT().CloneRepo(MOCK_REPO, LANG_BASE_DIR).Return(ERROR)
 			},
 		},
 		{
 			description: "Clone repo happy path",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(false, nil)
-				mfc.EXPECT().CreateDirectory(FULL_PATH).Return(nil)
-				mgc.EXPECT().CloneRepo(MOCK_REPO, FULL_PATH).Return(nil)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(false, nil)
+				mockFilesystem.EXPECT().CreateDirectory(FULL_PATH).Return(nil)
+				mockGitClient.EXPECT().CloneRepo(MOCK_REPO, LANG_BASE_DIR).Return(nil)
 			},
 			assertState: func(rs *repositoryState) bool {
 				return rs.path == FULL_PATH && rs.r.FullName == "repo" && rs.r.SshUrl == "repo.com" && rs.r.Name() == "repo" && !rs.hasLocalChanges
@@ -79,26 +80,26 @@ func TestRefresh(t *testing.T) {
 		},
 		{
 			description: "Forwards error from failed HasLocalChanges call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(true, nil)
-				mgc.EXPECT().HasLocalChanges(FULL_PATH).Return(false, ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(true, nil)
+				mockGitClient.EXPECT().HasLocalChanges(FULL_PATH).Return(false, ERROR)
 			},
 		},
 		{
 			description: "Forwards error from failed HasLocalChanges call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(true, nil)
-				mgc.EXPECT().HasLocalChanges(FULL_PATH).Return(false, ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(true, nil)
+				mockGitClient.EXPECT().HasLocalChanges(FULL_PATH).Return(false, ERROR)
 			},
 		},
 		{
 			description: "Repo with local changes happy path",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(true, nil)
-				mgc.EXPECT().HasLocalChanges(FULL_PATH).Return(true, nil)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(true, nil)
+				mockGitClient.EXPECT().HasLocalChanges(FULL_PATH).Return(true, nil)
 			},
 			assertState: func(rs *repositoryState) bool {
 				return rs.path == FULL_PATH && rs.r.FullName == "repo" && rs.r.SshUrl == "repo.com" && rs.r.Name() == "repo" && rs.hasLocalChanges
@@ -106,27 +107,27 @@ func TestRefresh(t *testing.T) {
 		},
 		{
 			description: "Forwards error from PullLatestChanges call",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(true, nil)
-				mgc.EXPECT().HasLocalChanges(FULL_PATH).Return(false, nil)
-				mgc.EXPECT().PullLatestChanges(FULL_PATH).Return(ERROR)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(true, nil)
+				mockGitClient.EXPECT().HasLocalChanges(FULL_PATH).Return(false, nil)
+				mockGitClient.EXPECT().PullLatestChanges(FULL_PATH).Return(ERROR)
 			},
 		},
 		{
 			description: "Repo without local changes happy path",
-			defineExpects: func(mghc *mock_interfaces.MockGithubClient, mgc *mock_interfaces.MockGitClient, mfc *mock_interfaces.MockFilesystemClient) {
-				mghc.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
-				mfc.EXPECT().Exists(FULL_PATH).Return(true, nil)
-				mgc.EXPECT().HasLocalChanges(FULL_PATH).Return(false, nil)
-				mgc.EXPECT().PullLatestChanges(FULL_PATH).Return(nil)
+			defineExpects: func() {
+				mockGithubClient.EXPECT().GetPrimaryLanguageForRepo(FULL_NAME).Return(PYTHON, nil)
+				mockFilesystem.EXPECT().Exists(FULL_PATH).Return(true, nil)
+				mockGitClient.EXPECT().HasLocalChanges(FULL_PATH).Return(false, nil)
+				mockGitClient.EXPECT().PullLatestChanges(FULL_PATH).Return(nil)
 			},
 			assertState: func(rs *repositoryState) bool {
 				return rs.path == FULL_PATH && rs.r.FullName == "repo" && rs.r.SshUrl == "repo.com" && rs.r.Name() == "repo" && !rs.hasLocalChanges
 			},
 		},
 	} {
-		tc.defineExpects(mockGithubClient, mockGitClient, mockFilesystem)
+		tc.defineExpects()
 		state, err := cmd.refresh(MOCK_REPO)
 
 		if err != nil && err.Error() != "failure" {
